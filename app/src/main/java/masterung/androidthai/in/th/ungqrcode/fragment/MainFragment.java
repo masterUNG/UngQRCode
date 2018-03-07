132578096c4a59c9d45d007101894b24520c48db
MasterUNG <phrombutr@gmail.com>
2018-03-07 15:37:38 +0700

    Create MainFragment Success

---
+++ app/src/main/java/masterung/androidthai/in/th/ungqrcode/fragment/MainFragment.java
@@ -0,0 +1,25 @@
+package masterung.androidthai.in.th.ungqrcode.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import masterung.androidthai.in.th.ungqrcode.R;
+
+/**
+ * Created by masterung on 7/3/2018 AD.
+ */
+
+public class MainFragment extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   // Main Class
