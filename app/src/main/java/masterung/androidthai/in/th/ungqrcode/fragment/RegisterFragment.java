8b030c19b5f8ca4b7461d77dedb54d82975ec712
MasterUNG <phrombutr@gmail.com>
2018-03-08 14:50:16 +0700

    Sign Up Success

--- app/src/main/java/masterung/androidthai/in/th/ungqrcode/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungqrcode/fragment/RegisterFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,10 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import masterung.androidthai.in.th.ungqrcode.MainActivity;
 import masterung.androidthai.in.th.ungqrcode.R;
 import masterung.androidthai.in.th.ungqrcode.utility.MyAlert;
+import masterung.androidthai.in.th.ungqrcode.utility.MyConstance;
+import masterung.androidthai.in.th.ungqrcode.utility.PostNewUserToServer;
 
 /**
  * Created by masterung on 8/3/2018 AD.
@@ -63,6 +67,31 @@ public class RegisterFragment extends Fragment{
 
         } else {
 //            No Space
+            try {
+
+                MyConstance myConstance = new MyConstance();
+                PostNewUserToServer postNewUserToServer = new PostNewUserToServer(getActivity());
+                postNewUserToServer.execute(
+                        nameString,
+                        userString,
+                        passwordString,
+                        myConstance.getUrlAddUser());
+                String result = postNewUserToServer.get();
+                Log.d("8MarchV1", "result ==> " + result);
+
+                if (Boolean.parseBoolean(result)) {
+                    Toast.makeText(getActivity(), "Welcome to App",
+                            Toast.LENGTH_SHORT).show();
+                    getActivity().getSupportFragmentManager().popBackStack();
+                } else {
+                    Toast.makeText(getActivity(), "Error Upload to Server",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         }   // if
 
---
+++ app/src/main/java/masterung/androidthai/in/th/ungqrcode/utility/MyConstance.java
@@ -0,0 +1,14 @@
+package masterung.androidthai.in.th.ungqrcode.utility;
+
+/**
+ * Created by masterung on 8/3/2018 AD.
+ */
+
+public class MyConstance {
+
+    private String urlAddUser = "http://androidthai.in.th/cent/addDataMaster.php";
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
