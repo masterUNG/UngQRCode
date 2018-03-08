71eba152a46da0f1432b94046102f11f2dab25cc
MasterUNG <phrombutr@gmail.com>
2018-03-08 10:38:49 +0700

    popToBack stack Success

--- app/src/main/java/masterung/androidthai/in/th/ungqrcode/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungqrcode/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package masterung.androidthai.in.th.ungqrcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.ungqrcode.MainActivity;
 import masterung.androidthai.in.th.ungqrcode.R;
 
 /**
@@ -14,6 +16,39 @@ import masterung.androidthai.in.th.ungqrcode.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    private void createToolbar() {
+
+//        Config Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title and SubTitle
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_register));
+
+//        Setup Navigator
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }
 
     @Nullable
     @Override
